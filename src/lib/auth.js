1b33f4c376ba6641db7a93f5bf3e8cf8f83a24bc
protapchakma <protapchakma43@gmail.com>
2026-05-05 22:25:21 +0600

    auth code update

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,20 +3,22 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import clientPromise from "./mongodb";
 
 export const auth = betterAuth({
-  database: mongodbAdapter(clientPromise, {
-    dbName: "SunCartDB",
-  }),
+    
+    database: mongodbAdapter(clientPromise, {
+        dbName: "SunCartDB",
+    }),
 
-  baseURL: process.env.BETTER_AUTH_URL,
+    baseURL: process.env.BETTER_AUTH_URL, 
+    secret: process.env.BETTER_AUTH_SECRET, 
 
-  emailAndPassword: {
-    enabled: true,
-  },
-
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    emailAndPassword: {
+        enabled: true,
     },
-  },
+
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        },
+    },
 });
