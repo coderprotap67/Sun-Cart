53e247b20904f98a77021894250964431cc0f78f
protapchakma <protapchakma43@gmail.com>
2026-05-05 14:15:51 +0600

    live url libk solved

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "https://sun-cart-enzr.vercel.app" 
+    baseURL: "https://sun-cart-psi.vercel.app",
 });
 
 
