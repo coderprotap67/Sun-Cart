28d5302f40f0dc43d7502c2ad0fbc67ca3909311
protapchakma <protapchakma43@gmail.com>
2026-05-05 22:53:59 +0600

    auth code changed

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,10 +2,11 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import clientPromise from "./mongodb";
 
+const client = await clientPromise;
+const db = client.db("SunCartDB");
+
 export const auth = betterAuth({
-  database: mongodbAdapter(clientPromise, {
-    dbName: "SunCartDB", 
-  }),
+  database: mongodbAdapter(db),
 
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
@@ -13,7 +14,7 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
-    minPasswordLength: 6, //
+    minPasswordLength: 6,
   },
 
   socialProviders: {
